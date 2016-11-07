fe3a78e8c2e1413baf3ba07fe5ac5be67b5c71fe
root <root@localhost.localdomain>
2016-11-07 14:21:56 -0500

    ping

--- Net_scan.py
+++ Net_scan.py
@@ -1,14 +1,23 @@
 import sys # Argumentos por linea de comandos
 import whois
+import os
 # whois de la maquina objetivo
-def who_is(domains):
-    u = whois.whois(domains)
-    print u.name
+def who_is(name_domain):
+    u = whois.whois(name_domain)
+    print u # Imprime toda la informacion
+
+# Ping
+def ping(name_domain):
+    response = os.system("ping -c 1 " + name_domain)
+    if response == 0:
+        print name_domain, 'is up!'
+    else:
+        print name_domain, 'is down!'
 
 def main():
     print "Bienvenido a net scan:"
-    domains = sys.argv[1]
-    who_is(domains)
+    name = sys.argv[1]
+    ping(name)
 
 if __name__ == "__main__":
     main()
